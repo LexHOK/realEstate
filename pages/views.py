00cb6244e58fd9dc649eda5b6d5886156b5202db
Unknown <grizzly.hok@gmail.com>
2018-10-15 21:58:12 -0500

    pages urls setup

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Hello Laura,</h1><br><p>You look beautiful pregnant. Do you know?</p>')
